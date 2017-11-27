69b31fb641ffb2b7bcdc5f23385213e22837a11c
Unknown <christsutamas@gmail.com>
2017-11-27 18:09:11 +0700

    ทำข้อที่1

--- lab_3_1.cpp
+++ lab_3_1.cpp
@@ -4,5 +4,14 @@ using namespace std;
 
 int main() {
   // เขียนโปรแกรมเพื่อแสดงผลตามที่กำหนด
+  cout << "MS."
+          "RATCHANOK"
+          "JUNJAMRAS"
+          "JAM"
+          "femal"
+          "1999,01,10"
+          "Play Game"
+          "single"
+            << endl;
   return 0;
 }
