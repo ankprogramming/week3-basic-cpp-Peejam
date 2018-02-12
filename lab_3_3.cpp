5514b3ddbf35a01198cbdd0c68b97a43fbfea2f8
Unknown <ratchanok1343@gmail.com>
2018-02-12 17:20:57 +0700

    เสร็จแล้ว

--- lab_3_3.cpp
+++ lab_3_3.cpp
@@ -3,14 +3,30 @@
 using namespace std;
 
 int main() {
-        const float pi + 3.14315926536
-        float r:
-            int k = 2 ,h+ 15:
-                cout << "Please enter radius : " ;
-                cin >> r;
-                cout <<"Area of Circle : " <<pi*r*r<<endl;
-                cout << "Circumference of Circle :" <<k*pi*r<<endl;
-                cout << "Cylindrical space :"<<pi*(r*r) *h<<endl;
+
+    const float pi = 3.14315926536;
+    float r;
+    float area;
+    int h=15;
+
+//Ç§ĄĹÁ
+    cout << "Please enter radius : ";
+    cin >> r;
+    area = pi*r*r;
+    cout << "Area of Circle :" << area << endl;
+//ŕĘéšĂÍÂ
+    cout << "Formula for circular circumference"<< endl;
+    cout << "Area of Circle : " << 2*pi*r<< endl;
+//ˇĂ§ĄĂĐşÍĄ
+    cout << "15 high cylinder formula"<< endl;
+    cout << "Area of Circle :" << pi*(r*r)*h<< endl;
+
+
+
+
+
+
+
 
   return 0;
 }
